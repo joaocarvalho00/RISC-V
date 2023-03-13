7e0a0be4c7329b458aa440b8943c7967dadc5804
joaocarvalho00 <joaoafonsocarvalho00@gmail.com>
2023-03-13 23:10:31 +0000

    set defines

---
+++ defines.v
@@ -0,0 +1,11 @@
+// Opcodes
+`define LUI 7'b0110111
+`define AUIPC 7'b0010111
+`define JAL 7'b1101111
+`define JALR 7'b1100111
+`define LOAD 7'b0000011
+`define STORE 7'b0100011
+`define OP 7'b0110011
+`define OP_IMM 7'b0010011
+`define BRANCH 7'b1100011
+`define SYSCALL 7'b1110011
