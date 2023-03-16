a15c76a1a9054d43c09d916463099e06f28ab6b6
joaocarvalho00 <joaoafonsocarvalho00@gmail.com>
2023-03-16 15:01:07 +0000

    clean code

--- src/reg.v
+++ src/reg.v
@@ -1,58 +1,42 @@
-
-
 module registersFile (
     input clk,
     input rst,
-    input [4:0] reg_addr1, reg_addr2, //address of the registers
-    input [4:0] writereg_addr, //write register address
+    input [4:0] reg_addr1, reg_addr2, // address of the registers
+    input [4:0] writereg_addr, // write register address
     input write_en,
     input [31:0] write_data,
-    output reg [31:0] reg_read1, reg_read2 //output from registers
+    output reg [31:0] reg_read1, reg_read2 // output from registers
 );
 
-reg [31:0] registers [31:0]; //32 registers de 32 bits
+reg [31:0] registers [31:0]; // 32 registers - 32 bits
 
 integer i;
 initial
 begin
     for (i=0; i<32; i=i+1) begin
-        registers[i] = 0;
+        registers[i] <= 0;
     end
 end
 
-//READ1
+// Read1
 always @(posedge clk or posedge rst) begin
-
-    if(rst) begin
-        reg_read1 = 32'b0;
-        reg_read2 = 32'b0;
-    end
-  
-    reg_read1 <= registers[reg_addr1]; //Se for immediate só lê um registo rs rt rd
-    //$display("reg_read1 do reg.v = %d", reg_read1);
-
+    if(rst)
+        reg_read1 <= 32'b0;
+    else
+    reg_read1 <= registers[reg_addr1]; // If immediate only reads addr1
 end
 
-//READ2
-always @(posedge clk) begin
-
-
+// Read2
+always @(posedge clk or posedge rst) begin
+    if(rst)
+        reg_read2 <= 32'b0;
+    else
     reg_read2 <= registers[reg_addr2];
-    //$display("reg_read2 reg.v = %d", reg_read2);
-
 end
 
-//WRITE
+// Write
 always @(posedge clk) begin
-
-    if(write_en) begin
+    if(write_en)
         registers[writereg_addr] <= write_data; //Se for immediate pôr o if para nao ler dois registos
-    end 
-
 end
-
-
-
-    
-    
 endmodule
